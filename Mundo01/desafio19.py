fb1c87d7afc2425a02fc1587eaed085b78a00e14
Guilherme <macarioguilherme8534@gmail.com>
2025-10-14 20:15:28 -0300

    Commit!

--- Mundo01/desafio19.py
+++ Mundo01/desafio19.py
@@ -3,7 +3,7 @@ import random
 n1 = input("Nome do primeiro aluno: ")
 n2 = input("Nome do segundo aluno: ")
 n3 = input("Nome do terceiro aluno: ")
-n4 = input("Nome do quatro aluno: ")
+n4 = input("Nome do quatro aluno:: ")
 array = [n1,n2,n3,n4]
 escolhido = random.choice(array)
 print(f"O aluno escolhido foi {escolhido}")
