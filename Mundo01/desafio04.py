479e6159115e8909e550b83db70939b42a8166f8
Guilherme <macarioguilherme8534@gmail.com>
2025-10-14 20:17:03 -0300

    Commit!

--- Mundo01/desafio04.py
+++ Mundo01/desafio04.py
@@ -1,3 +1,3 @@
 #Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor
 numero = int(input("Digite seu núméro, que mostraremos o seu antecessor e seu sucessor: "))
-print(f" O antecessor de {numero} é {numero -1} e o seu sucessor é {numero + 1}")
+print(f" O antecessor de numero} é {numero -1} e o seu sucessor é {numero + 1}")
